208441809ca533e08689b7bdf023c69426075bd9
Guilherme Fortuna <52174388+TronzinhoxD@users.noreply.github.com>
2024-06-25 16:48:36 -0300

    Quais que eu esqueço do commit
    fui embora tchau

--- src/app/modules/admin/consultar-risco/consultar-risco.component.ts
+++ src/app/modules/admin/consultar-risco/consultar-risco.component.ts
@@ -1,4 +1,4 @@
-import { Component, ApplicationModule } from '@angular/core';
+import { Component, ApplicationModule, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCommonModule, MatOptionModule } from '@angular/material/core';
 import { MatDividerModule } from '@angular/material/divider';
@@ -8,18 +8,11 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { FuseHighlightComponent } from '@fuse/components/highlight';
 import { fuseAnimations } from '@fuse/animations';
 import { MatInputModule } from '@angular/material/input';
-import {MatTableModule} from '@angular/material/table';
-
-export interface PeriodicElement {
-  risco: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {risco: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae mauris non nisi porta lacinia.'},
-  {risco: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae mauris non nisi porta lacinia.'},
-  {risco: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae mauris non nisi porta lacinia.'},
-  {risco: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae mauris non nisi porta lacinia.'},
-];
+import { MatTableModule } from '@angular/material/table';
+import { ObjetivosEstrategicosService, objetivosEstrategi } from 'app/services/ObjetivosEstrategicos.service';
+import { MatListModule } from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'consultarRisco',
@@ -36,7 +29,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
     MatTabsModule,
     MatTableModule,
     FuseHighlightComponent,
-    
+    MatListModule,
+    FormsModule,
+    CommonModule
+
   ],
   templateUrl: './consultar-risco.component.html',
   styleUrl: './consultar-risco.component.scss',
@@ -44,9 +40,41 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 
-export class ConsultarRiscoComponent {
+export class ConsultarRiscoComponent implements OnInit {
 
-  displayedColumns: string[] = ['risco'];
-  dataSource = ELEMENT_DATA;
+
+  objetivosEstrategicos: objetivosEstrategi[] = [];
+  errorMessage: string = '';
+  objetivoEstrategico : string = '';
+
+  constructor(private objetivosEstrategicosService: ObjetivosEstrategicosService) { }
+
+  ngOnInit(): void {
+    this.objetivosEstrategicosService.pegarObjetivos().subscribe(
+      (data: objetivosEstrategi[]) => {
+        this.objetivosEstrategicos = data;
+      },
+      (error) => {
+        this.errorMessage = 'Ainda não possui objetivos estratégicos cadastrados!';
+        console.error('Erro ao carregar os objetivos estratégicos:', error);
+      }
+    );
+  }
+
+  salvarObjetivo() {
+    const objetivo: objetivosEstrategi = {
+      objetivoEstrategico: this.objetivoEstrategico,
+
+    };
+
+    this.objetivosEstrategicosService.criarObjetivos(objetivo).subscribe(
+      (response) => {
+
+      },
+      (error) => {
+        console.error('Erro ao Criar Usuario:', error);
+      }
+    );
+  }
 }
 
---
+++ src/app/services/ObjetivosEstrategicos.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export type objetivosEstrategi = {
+  objetivoEstrategico: string;
+}; 
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ObjetivosEstrategicosService {
+  apiUrl = 'http://localhost:3000/teste';
+  constructor(private http: HttpClient) { }
+
+  pegarObjetivos(): Observable<objetivosEstrategi[]> {
+    return this.http.get<objetivosEstrategi[]>(this.apiUrl).pipe(
+      catchError(error => {
+        console.error('Erro ao buscar os objetivos estratégicos:', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  criarObjetivos(objetivos : objetivosEstrategi){
+    return this.http.post(this.apiUrl, objetivos);
+  }
+}
